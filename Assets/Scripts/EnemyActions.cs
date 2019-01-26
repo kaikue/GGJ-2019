69fcf75ec5f2738e5667d84b9837692901eee1d3
Chris Schultz <LightRat701@gmail.com>
2019-01-26 12:37:34 -0500

    Added enemy actions script. Fires at player.

---
+++ Assets/Scripts/EnemyActions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyActions : MonoBehaviour
+{
+    public GameObject player;
+    private BoxCollider2D playerBox;
+
+    public float projectile_speed;
+    public float reload_speed;
+    public GameObject projectile;
+    private bool gunLoaded = true;
+
+    private float lastLook = -1;    //start left towards where player starts
+    private const float SPRITE_SIZE_OFFSET = 1.5f; //TEMP
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBox = player.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO - MOVEMENT
+
+        //FIRING
+        //TODO - actually get direction based on movement
+
+        if(gunLoaded && playerBox.bounds.Contains(
+            new Vector3(playerBox.bounds.center.x, transform.position.y, playerBox.bounds.center.z)))
+        {
+            //shoot
+            GameObject shot = Instantiate(projectile, new Vector2(transform.position.x + lastLook * SPRITE_SIZE_OFFSET, transform.position.y), Quaternion.identity);
+            Rigidbody2D rb = shot.GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+            rb.velocity = transform.right * projectile_speed * lastLook;
+            Debug.Log("velocity=" + rb.velocity);
+            gunLoaded = false;
+            StartCoroutine(Reload());
+        }
+        
+    }
+
+    public IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(reload_speed);
+        gunLoaded = true;
+    }
+}
---
+++ Assets/Scripts/PlaceEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceEnemy : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+    public int enemyCount;
+    public float projectile_speed;
+    public float reload_speed;
+    public GameObject projectile;
+
+    public float topLimit;
+    public float bottomLimit;
+    public float leftLimit;
+    public float rightLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < enemyCount; ++i)
+        {
+            float x = Random.Range(leftLimit, rightLimit);
+            float y = Random.Range(bottomLimit, topLimit);
+            GameObject newEnemy = Instantiate(enemy, new Vector3(x, y, 0), Quaternion.identity);
+            EnemyActions actionsScript = newEnemy.GetComponent<EnemyActions>();
+            actionsScript.player = player;
+            actionsScript.projectile = projectile;
+            actionsScript.projectile_speed = projectile_speed;
+            actionsScript.reload_speed = reload_speed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
